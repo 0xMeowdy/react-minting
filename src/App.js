410866b1b8a4f3d73b0eebed2b1b2edc1dfec9d8
Mike Johnston <mike.david.johnston@gmail.com>
2021-10-25 20:40:58 -0600

    init boilerplate react and tailwind

---
+++ src/App.js
@@ -0,0 +1,21 @@
+function App() {
+	return (
+		<div>
+			<div>
+				<div className='max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8'>
+					<h2 className='text-3xl font-extrabold text-blue-700 sm:text-4xl'>
+						<span className='block'>React Minting</span>
+					</h2>
+					<p className='mt-4 text-lg leading-6 text-gray-600'>
+						Description of collection goes here.
+					</p>
+					<button className='mt-8 w-full inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-50 bg-blue-600 hover:bg-blue-800 sm:w-auto'>
+						Mint
+					</button>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
