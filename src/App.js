b5ad82b84b0cfc33c4a9ea5f693172623d96c99c
Mike Johnston <mike.david.johnston@gmail.com>
2021-10-25 20:49:31 -0600

    add check for ethereum wallet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
+import { useEffect } from 'react'
+
 function App() {
+	const checkIfWalletIsConnected = () => {
+		/*
+		 * First make sure we have access to window.ethereum
+		 */
+		const { ethereum } = window
+
+		if (!ethereum) {
+			console.log('Make sure you have metamask!')
+			return
+		} else {
+			console.log('We have the ethereum object', ethereum)
+		}
+	}
+
+	/*
+	 * This runs our function when the page loads.
+	 */
+	useEffect(() => {
+		checkIfWalletIsConnected()
+	}, [])
+
 	return (
 		<div>
 			<div>
